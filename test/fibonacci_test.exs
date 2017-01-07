d738a8b69c9953e596f9a0dc1556453395449d96
Kayleigh Rix <kayleigh.rix@outplay.com>
2017-01-07 03:49:52 +0000

    [add] more tests

--- test/fibonacci_test.exs
+++ test/fibonacci_test.exs
@@ -11,4 +11,9 @@ defmodule FibonacciTest do
     fibs = Fibonacci.get_n_fibs(10)
     assert fibs === [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
   end
+
+  test "get_n_fibs provides 100 elements" do
+    fibs_length = length(Fibonacci.get_n_fibs(100))
+    assert fibs_length == 100
+  end
 end
