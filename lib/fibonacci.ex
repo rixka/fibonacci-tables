e5da780f1e6762fb3395400af59b2e076667afb2
Kayleigh Rix <kayleigh.rix@outplay.com>
2017-01-07 00:30:40 +0000

    [add] complete fibonacci solution

--- lib/fibonacci.ex
+++ lib/fibonacci.ex
@@ -6,33 +6,50 @@ defmodule Fibonacci do
   @doc """
     Provides the fibonacci sequence as an array.
     The `n` argument indicates the length of the array.
-
-  ## Examples
-
-      iex> Fibonacci.get_n_fibs(5)
-      [1, 1, 2, 3, 5]
-
   """
   def get_n_fibs(n) do
-    n
+    fib_loop([1], n)
   end
 
   @doc """
-    Provides the next fibonacci number in the sequence.
-    The `fibs` argument is the fibonacci sequence as an array.
+    Recursively loops to generate the desired fibonacci
+    sequence length.
+    The 'fibs` argument is the existing fibonacci sequence
+    and the `n` argument is the desired length of the array.
 
   ## Examples
 
-      iex> Fibonacci.next_fib([1, 1, 2])
-      [3]
+      iex> Fibonacci.fib_loop([1], 5)
+      [1, 1, 2, 3, 5]
+
   """
-  def next_fib(fibs) do
+  def fib_loop(fibs, n) do
     cond do
-      length(fibs) == 1 ->
-        [1]
+      length(fibs) == n ->
+        fibs
+      true ->
+        fibs
+        |> add_next_fib
+        |> fib_loop(n)
+    end
+  end
+
+  @doc """
+    Appends the next fibonacci number to the sequence.
+    The `fibs` argument is the old fibonacci sequence as an array.
+
+  ## Examples
+
+      iex> Fibonacci.add_next_fib([1, 1, 2])
+      [1, 1, 2, 3]
+  """
+  def add_next_fib(fibs) do
+    cond do
+      fibs == [1] ->
+        [1, 1]
       true ->
         [pen, last] = Enum.take(fibs, -2)
-        [pen + last]
+        fibs ++ [pen + last]
     end
   end
 end
