17b0dbb111da8d308df4eb9ee0af6083e71ba7fb
Kayleigh Rix <kayleigh.rix@outplay.com>
2017-01-07 00:34:29 +0000

    [add] full pass fib tests

--- test/fibonacci_test.exs
+++ test/fibonacci_test.exs
@@ -1,4 +1,14 @@
 defmodule FibonacciTest do
   use ExUnit.Case
   doctest Fibonacci
+
+  test "get_n_fibs provides first 2 elements" do
+    fibs = Fibonacci.get_n_fibs(2)
+    assert fibs === [1, 1]
+  end
+
+  test "get_n_fibs provides first 10 elements" do
+    fibs = Fibonacci.get_n_fibs(10)
+    assert fibs === [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+  end
 end
